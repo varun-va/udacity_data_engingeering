597033d985de791bba70a11cc5ef72b5dafbf3c8
VarunV <22299622+varunvasud@users.noreply.github.com>
2021-04-28 00:36:22 -0700

    removed absolute path

--- 5_Airflow_Data_Pipelines/plugins/operators/create_table.py
+++ 5_Airflow_Data_Pipelines/plugins/operators/create_table.py
@@ -17,7 +17,8 @@ class CreateTableOperator(BaseOperator):
         redshift = PostgresHook(postgres_conn_id = self.redshift_conn_id)
 
         self.log.info('Executing creating tables in Redshift.')
-        queries =  open('/home/workspace/airflow/create_tables.sql', 'r').read()
+        #queries =  open('/home/workspace/airflow/create_tables.sql', 'r').read()
+        queries =  open('../../create_tables.sql', 'r').read()
         redshift.run(queries)
         
         self.log.info("Tables created ")
